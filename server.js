250971e17535f361e289951503f5e9a4ad48f045
John Dawes <jdawes@bluebitmedia.com>
2018-08-10 11:38:00 -0600

    Update profile

--- server.js
+++ server.js
@@ -26,15 +26,16 @@ const db = knex({
 const app = express();
 
 app.use(morgan('combined'));
-app.use(cors())
+app.use(cors());
 app.use(bodyParser.json());
 
-app.get('/', (req, res) => { res.send("API IS RUNNING") })
-app.post('/signin', signin.handleSignin(db, bcrypt))
-app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
-app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db) })
-app.put('/image', (req, res) => { image.handleImage(req, res, db) })
-app.post('/imageurl', (req, res) => { image.handleApiCall(req, res) })
+app.get('/', (req, res) => { res.send("API IS RUNNING") });
+app.post('/signin', signin.handleSignin(db, bcrypt));
+app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) });
+app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db) });
+app.post('/profile/:id', (req, res) => { profile.handleProfileUpdate(req, res, db) });
+app.put('/image', (req, res) => { image.handleImage(req, res, db) });
+app.post('/imageurl', (req, res) => { image.handleApiCall(req, res) });
 
 app.listen(3000, () => {
     console.log('app is running on port 3000');
