0796432f340c01432f6596c2dce1af92228ad904
John Dawes <jdawes@bluebitmedia.com>
2018-08-10 20:24:24 -0600

    JWT and user profile improvements

--- controllers/signin.js
+++ controllers/signin.js
@@ -5,57 +5,71 @@ const redis = require('redis');
 const redisClient = redis.createClient(process.env.REDIS_URI);
 
 const handleSignin = (db, bcrypt, req, res) => {
-   const { email, password } = req.body;
-   if (!email || !password) {
-      return Promise.reject('incorrect form submission');
-   }
+    const { email, password } = req.body;
+    if (!email || !password) {
+        return Promise.reject('incorrect form submission');
+    }
 
-   return db.select('email', 'hash').from('login')
-      .where('email', '=', email)
-      .then(data => {
-         const isValid = bcrypt.compareSync(password, data[0].hash);
-         if (isValid) {
-            return db.select('*').from('users')
-               .where('email', '=', email)
-               .then(user => user[0])
-               .catch(err => Promise.reject('unable to get user'))
-         } else {
-            Promise.reject('wrong credentials')
-         }
-      })
-      .catch(err => Promise.reject('wrong credentials'))
+    return db.select('email', 'hash').from('login')
+        .where('email', '=', email)
+        .then(data => {
+            const isValid = bcrypt.compareSync(password, data[0].hash);
+            if (isValid) {
+                return db.select('*').from('users')
+                    .where('email', '=', email)
+                    .then(user => user[0])
+                    .catch(err => Promise.reject('unable to get user'))
+            } else {
+                Promise.reject('wrong credentials')
+            }
+        })
+        .catch(err => Promise.reject('wrong credentials'))
 }
 
-const getAuthTokenId = () => {
-   console.log('auth ok');
+const getAuthTokenId = (req, res) => {
+    const { authorization } = req.headers;
+
+    return redisClient.get(authorization, (err, reply) => {
+        if (err || !reply) {
+            return res.status(400).json('Unauthorized')
+        } else {
+            return res.json({ id: reply });
+        }
+    })
 }
 
 const signToken = (email) => {
-   const jwtPayload = { email };
-   return jwt.sign(jwtPayload, process.env.JWT_SECRET, { expiresIn: '2 days' }); //TODO: move to JWT_SECRET
+    const jwtPayload = { email };
+    return jwt.sign(jwtPayload, process.env.JWT_SECRET, { expiresIn: '2 days' }); //TODO: move to JWT_SECRET
+}
+
+const setToken = (key, value) => {
+    return Promise.resolve(redisClient.set(key, value));
 }
 
 const createSession = (user) => {
-   // JWT Token, return user data
-   const { email, id } = user;
-   const token = signToken(email);
-   return { success: 'true', userId: id, token: token }
+    // JWT Token, return user data
+    const { email, id } = user;
+    const token = signToken(email);
+    return setToken(token, id)
+        .then(() => ({ success: 'true', userId: id, token: token }))
+        .catch(err => console.log)
 }
 
 const handleAuthentication = (db, bcrypt) => (req, res) => {
-   const { authorization } = req.headers;
+    const { authorization } = req.headers;
 
-   // Is there a token already? If so get the user profile, else handlesign with email/pass
-   return authorization ?
-      getAuthTokenId() :
-      handleSignin(db, bcrypt, req, res)
-         .then(data => {
-            return data.id && data.email ? createSession(data) : Promise.reject(data);
-         })
-         .then(session => res.json(session))
-         .catch(err => res.status(400).json(err));
+    // Is there a token already? If so get the user profile, else handlesign with email/pass
+    return authorization ?
+        getAuthTokenId(req, res) :
+        handleSignin(db, bcrypt, req, res)
+            .then(data => {
+                return data.id && data.email ? createSession(data) : Promise.reject(data);
+            })
+            .then(session => res.json(session))
+            .catch(err => res.status(400).json(err));
 }
 
 module.exports = {
-   handleAuthentication: handleAuthentication
+    handleAuthentication: handleAuthentication
 }
